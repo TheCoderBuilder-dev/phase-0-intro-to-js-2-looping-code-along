a2ecc0ef8e3b7f3488d9bdae82943574edd9dfcb
Brian Munene Mwirigi <brianinesh@gmail.com>
2025-03-13 10:03:03 +0300

    CompletedLab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Example usage:
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example usage:
+countDown(10);
+
+
